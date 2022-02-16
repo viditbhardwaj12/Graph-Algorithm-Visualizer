6ffb3a53b7010847c4753f65e00e4dae94c28f2c
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2022-02-16 10:09:39 +0530

    Add files via upload

---
+++ src/components/DrawGraph/Instructions.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Instructions() {
+  return (
+    <ul className='instructions'>
+      <h2>Instructions</h2>
+      <li>Click in an empty space to create a node</li>
+      <li>Click a node and then click another to create an edge</li>
+      <li>Drag nodes by pressing and releasing</li>
+      <li>Click a node or an edge and then press "Delete" to remove them</li>
+      <li>Double click an edge to change its weight (if weights are activated)</li>
+      <li>Yellow color represents current node, blue color represent visited node,red edge rep discrded edge and blue node rep visited edge </li>
+    </ul>
+  );
+}
+// done
